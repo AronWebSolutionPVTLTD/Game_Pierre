c3d9a700ff3fd7e7251d6351743f4065f2851fb2
Shivam-Moudgil <shivammoudgil358@gmail.com>
2024-01-19 16:28:11 +0530

    --check

--- server/router/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controller/user");
-router.post("/signup", userController.signup);
-router.post("/login", userController.login);
-router.post("/forgotpassword", userController.forgotpassword);
-router.post("/resetpassword", userController.resetpassword);
-
-module.exports = router;
